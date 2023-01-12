2d355b4e70fc1f7e929cc5f1d416b9845d4df838
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2023-01-11 23:37:13 -0300

    commit ...

---
+++ doc/new.js
@@ -0,0 +1,38 @@
+function User(nome, email){
+    this.nome = nome
+    this.email = email
+
+    this.exebirInfo = function(){
+        return `${this.nome}, ${this.email}`
+    }
+}
+
+// const novoUser = new User('juliana', 'j@j.com')
+// console.log(novoUser.exebirInfo())
+
+// function Admim( role){
+//     User.call(this, 'juliana', 'j@j.com')
+//     this.role = role || 'estudante'
+// }
+
+// Admim.prototype = Object.create(User.prototype)
+// const novoUser = new Admim('Admim')
+// console.log(novoUser.exebirInfo())
+// console.log(novoUser.role)
+
+
+const user = {
+    init: function(nome,email, telefone){
+        this.nome = nome,
+        this.email = email,
+        this.telefone = telefone
+    },
+    exebirInfo: function(){
+        return this.nome, email, telefone
+    }
+}
+
+const novoUser = Object.create(user)
+novoUser.init('bigp', 'bigp@p.com')
+console.log(novoUser.exebirInfo())
+// console.log(user.isPrototypeOf(novoUser))
