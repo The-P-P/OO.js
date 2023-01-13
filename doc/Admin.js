178f2fc2d554e00e302c3200d69b8af23c9e26ef
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2023-01-12 21:45:41 -0300

    commit ....

--- doc/Admin.js
+++ doc/Admin.js
@@ -4,8 +4,11 @@ class Admin extends User{
     constructor(nome, email, nascimento, role = 'admin', ativo = true){
         super(nome, email, nascimento, role, ativo)
     }
+
+    criarCurso(nomeCurso, vagas){
+        return`Curso de ${nomeCurso} criado como ${vagas} vagas`
+    }
 }
 
 const novoAdmin = new Admin('paulo', 'A@a.com', '2022-01-01')
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.criarCurso('JS', 20))
